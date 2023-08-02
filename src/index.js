793830f0befa2e374f37ff1d0d926736cae08497
Plouis53 <Plouis53@gmail.com>
2023-08-01 20:04:28 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./blocks/app.css";
+import App from "./blocks/App.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
