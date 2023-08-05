8cc0ccd60c48249322a46c07c5da5c42ff1b24ce
Plouis53 <Plouis53@gmail.com>
2023-08-05 19:28:49 -0400

    added auth files

---
+++ src/utils/auth.js
@@ -0,0 +1,43 @@
+import { baseUrl, checkResponse } from "../utils/constants";
+
+export const signUp = (email, password, name) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, name }),
+  })
+    .then((res) => checkResponse(res))
+    .then((res) => res);
+};
+
+export const signIn = (email, password) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => checkResponse(res))
+    .then((data) => {
+      if (data) {
+        localStorage.setItem("jwt", data.token);
+        return data;
+      }
+    });
+};
+
+export const checkToken = (token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => checkResponse(res))
+    .then((data) => data);
+};
