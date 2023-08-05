1d070bfcd7b0f80c7fe7f272e608b2fb28877b47
Plouis53 <Plouis53@gmail.com>
2023-08-04 21:39:48 -0400

    added getTempArticles

--- src/components/NewsCardListSaved.js
+++ src/components/NewsCardListSaved.js
@@ -1,17 +1,19 @@
 import React from "react";
 import NewsCardSaved from "./NewsCardSaved";
-import { getNews } from "../utils/newsApi";
+import { getTempArticles } from "../utils/newsApi";
 
 const NewsCardListSaved = ({ isLoggedIn }) => {
   const [newsCards, setNewsCards] = React.useState([]);
 
-  getNews("Hello")
-    .then((data) => {
-      setNewsCards(data.articles);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  React.useEffect(() => {
+    getTempArticles("Hello")
+      .then((data) => {
+        setNewsCards(data.articles);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <section className="news">
--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -5,3 +5,9 @@ export const getNews = (input) => {
     `https://newsapi.org/register/success=${input}&apiKey=03163e8f2a6b45419e69c39fbacf5fe2=${getLastWeek()}&to=${getCurrentDate}&pageSize=100`
   ).then((res) => checkResponse(res));
 };
+
+export const getTempArticles = (input) => {
+  return fetch(
+    `https://newsapi.org/register/success=${input}&apiKey=03163e8f2a6b45419e69c39fbacf5fe2=&pageSize=5`
+  ).then((res) => checkResponse(res));
+};
