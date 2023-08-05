a214ac5a983b82ade23d4b82ba49fe5350003aca
Plouis53 <Plouis53@gmail.com>
2023-08-05 19:36:57 -0400

    addnew files and include minor updates

---
+++ src/utils/mainApi.js
@@ -0,0 +1,39 @@
+import { baseUrl, checkResponse } from "./constants";
+
+export const getArticles = (token) => {
+  return fetch(`${baseUrl}/articles`, {
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then((res) => checkResponse(res));
+};
+
+export const addArticle = (card, token) => {
+  return fetch(`${baseUrl}/articles`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      keyword: card.keyword,
+      title: card.title,
+      text: card.description,
+      date: card.publishedAt.slice(0, 10),
+      source: card.source.name,
+      link: card.url,
+      image: card.urlToImage,
+    }),
+  }).then((res) => checkResponse(res));
+};
+
+export const removeArticle = (id, token) => {
+  return fetch(`${baseUrl}/articles/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then((res) => checkResponse(res));
+};
