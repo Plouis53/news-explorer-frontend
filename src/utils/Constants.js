5cbc7afe60ea3616f5207d9935fbe0567bd5d115
Plouis53 <Plouis53@gmail.com>
2023-08-15 22:38:53 -0400

    updates

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,5 @@
 // export const baseUrl =
 //   // "https://my-json-server.typicode.com/Plouis53/news-explorer-frontend";
-
 //   "http://localhost:3001";
 
 export const APIkey = "03163e8f2a6b45419e69c39fbacf5fe2";
@@ -27,4 +26,4 @@ export const getCurrentDate = () => {
 export const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://api.newsquest.mooo.com"
-    : "http://localhost:3001";
+    : "http://localhost:3000";
