de4df98701631eb2d8784a617e7dc66965c8c11f
Plouis53 <Plouis53@gmail.com>
2023-08-15 22:45:09 -0400

    idk

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-export const baseUrl =
-  // "https://my-json-server.typicode.com/Plouis53/news-explorer-frontend";
-  "http://localhost:3001";
+// export const baseUrl =
+//   // "https://my-json-server.typicode.com/Plouis53/news-explorer-frontend";
+//   "http://localhost:3001";
 
 export const APIkey = "03163e8f2a6b45419e69c39fbacf5fe2";
 
@@ -23,7 +23,7 @@ export const getCurrentDate = () => {
   return currentDate.toLocaleString();
 };
 
-// export const baseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "https://api.newsquest.mooo.com"
-//     : "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.newsquest.mooo.com"
+    : "http://localhost:3001";
