da0fe23d5afe705f288126dc57947f67d4748441
Plouis53 <Plouis53@gmail.com>
2023-08-22 13:20:01 -0400

    added menu to mobile setting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,26 +13,10 @@ const Navigation = ({
   handleMobileClick,
   handleSignout,
 }) => {
-  const [homeClass, setHomeClass] = React.useState("");
-  const [articleClass, setArticleClass] = React.useState("");
-  const [color, setColor] = React.useState("");
-
   const activeModal = useContext(ActiveModalContext);
   const currentUser = useContext(CurrentUserContext);
 
   const userData = currentUser.data ? currentUser.data : { name: "" };
-
-  React.useEffect(() => {
-    if (isHomeActive) {
-      setHomeClass("active");
-      setArticleClass("inactive");
-      setColor(logoutWhite);
-    } else {
-      setHomeClass("inactive");
-      setArticleClass("active");
-      setColor(logout);
-    }
-  }, []);
 
   return (
     <div className={`nav nav_${theme}`}>
@@ -50,7 +34,9 @@ const Navigation = ({
       <div className="nav__right">
         <NavLink
           to="/"
-          className={`nav__home nav__link nav__highlight-${homeClass}_${theme} nav__link-active`}
+          className={`nav__home nav__link ${
+            isHomeActive ? "nav__highlight-active" : "nav__highlight-inactive"
+          }_${theme} nav__link-active`}
         >
           Home
         </NavLink>
@@ -58,7 +44,11 @@ const Navigation = ({
           <>
             <NavLink
               to="/saved-articles"
-              className={`nav__articles nav__link nav__highlight-${articleClass}_${theme} nav__link-${articleClass}`}
+              className={`nav__articles nav__link ${
+                isHomeActive
+                  ? "nav__highlight-inactive"
+                  : "nav__highlight-active"
+              }_${theme} nav__link-inactive`}
             >
               Saved articles
             </NavLink>
@@ -68,7 +58,7 @@ const Navigation = ({
             >
               {userData.name}
               <img
-                src={color}
+                src={isHomeActive ? logoutWhite : logout}
                 alt="Signout Button"
                 className="nav__signout-image"
               />
@@ -88,3 +78,94 @@ const Navigation = ({
 };
 
 export default Navigation;
+
+// import React, { useContext } from "react";
+// import { NavLink } from "react-router-dom";
+// import logout from "../images/logout.svg";
+// import logoutWhite from "../images/logoutWhite.svg";
+// import ActiveModalContext from "../contexts/ActiveModalContext";
+// import CurrentUserContext from "../contexts/CurrentUserContext";
+
+// const Navigation = ({
+//   onSigninClick,
+//   theme,
+//   isHomeActive,
+//   isLoggedIn,
+//   handleMobileClick,
+//   handleSignout,
+// }) => {
+//   const [homeClass, setHomeClass] = React.useState("");
+//   const [articleClass, setArticleClass] = React.useState("");
+//   const [color, setColor] = React.useState("");
+
+//   const activeModal = useContext(ActiveModalContext);
+//   const currentUser = useContext(CurrentUserContext);
+
+//   const userData = currentUser.data ? currentUser.data : { name: "" };
+
+//   React.useEffect(() => {
+//     if (isHomeActive) {
+//       setHomeClass("active");
+//       setArticleClass("inactive");
+//       setColor(logoutWhite);
+//     } else {
+//       setHomeClass("inactive");
+//       setArticleClass("active");
+//       setColor(logout);
+//     }
+//   }, []);
+
+//   return (
+//     <div className={`nav nav_${theme}`}>
+//       <NavLink to="/" className="nav__logo nav__link-active">
+//         News Explorer
+//       </NavLink>
+//       {activeModal === "signup" || activeModal === "signin" ? null : (
+//         <button
+//           className={`nav__mobile ${
+//             isHomeActive ? "mobile-icon-visible" : ""
+//           } nav__mobile_${theme}`}
+//           onClick={handleMobileClick}
+//         />
+//       )}
+//       <div className="nav__right">
+//         <NavLink
+//           to="/"
+//           className={`nav__home nav__link nav__highlight-${homeClass}_${theme} nav__link-active`}
+//         >
+//           Home
+//         </NavLink>
+//         {isLoggedIn ? (
+//           <>
+//             <NavLink
+//               to="/saved-articles"
+//               className={`nav__articles nav__link nav__highlight-${articleClass}_${theme} nav__link-${articleClass}`}
+//             >
+//               Saved articles
+//             </NavLink>
+//             <button
+//               className={`nav__signout-button nav__button_${theme}`}
+//               onClick={handleSignout}
+//             >
+//               {userData.name}
+//               <img
+//                 src={color}
+//                 alt="Signout Button"
+//                 className="nav__signout-image"
+//               />
+//             </button>
+//           </>
+//         ) : (
+//           <button
+//             className={`nav__signin nav__button_${theme} nav__button`}
+//             onClick={onSigninClick}
+//           >
+//             Sign in
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navigation;
