2f1cca401af65cd5fe97b6807b0021c0acfa5770
Plouis53 <Plouis53@gmail.com>
2023-08-24 00:13:16 -0400

    reinstalled npm and added eslintrc/f

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,10 @@ module.exports = {
   extends: ["eslint:recommended", "airbnb-base", "prettier"],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 2022, // Change this to a valid ECMAScript version
   },
-  rules: {},
+  rules: {
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
 };
