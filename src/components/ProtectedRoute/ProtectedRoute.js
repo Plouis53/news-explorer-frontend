6f06360b8124b155884fdc0ad97476646516c498
Plouis53 <Plouis53@gmail.com>
2023-08-30 11:55:30 -0400

    New updates

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,31 +1,13 @@
-// import React from 'react';
-// import { Route, Navigate } from 'react-router-dom';
-
-// function ProtectedRoute({ children, isLoggedIn }) {
-//   return <Route>{isLoggedIn ? children : <Navigate to={'/saved-articles'} />}</Route>;
-// }
-
-// export default ProtectedRoute;
-
-// import React from 'react';
-// import { Navigate } from 'react-router-dom';
-
-// const ProtectedRoute = ({ isLoggedIn, children }) => {
-//   if (!isLoggedIn) {
-//     console.log('signup');
-//     return <Navigate to="/" replace />;
-//   }
-
-//   return children;
-// };
-
-// export default ProtectedRoute;
-
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-function ProtectedRoute({ isLoggedIn, children, ...props }) {
-  return <Route {...props}>{isLoggedIn !== null ? children : <Navigate to="/" />}</Route>;
-}
+const ProtectedRoute = ({ isLoggedIn, children }) => {
+  if (!isLoggedIn) {
+    console.log('signup');
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 export default ProtectedRoute;
