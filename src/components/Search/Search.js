742557027b19cc240dfa75773720675c999c21da
Plouis53 <Plouis53@gmail.com>
2023-09-07 01:18:15 -0400

    first updates

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,13 +3,15 @@ import SearchForm from '../SearchForm/SearchForm';
 
 const Search = ({ handleSearchSubmit, setKeyword }) => {
   return (
-    <section className="search">
-      <h1 className="search__title">What's going on in the world?</h1>
-      <p className="search__info">
-        Find the latest news on any topic and save them in your personal account.
-      </p>
-      <SearchForm handleSearchSubmit={handleSearchSubmit} setKeyword={setKeyword} />
-    </section>
+    <div className="search-home__content">
+      <div className="search-home__text">
+        <h1 className="search-home__title">What's going on in the world?</h1>
+        <h2 className="search-home__info">
+          Find the latest news on any topic and save them in your personal account.
+        </h2>
+        <SearchForm handleSearchSubmit={handleSearchSubmit} setKeyword={setKeyword} />
+      </div>
+    </div>
   );
 };
 
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-
-
 const SearchForm = ({ handleSearchSubmit, setKeyword }) => {
   const {
     register,
@@ -19,28 +17,28 @@ const SearchForm = ({ handleSearchSubmit, setKeyword }) => {
 
   return (
     <form className="search__form" onSubmit={handleSubmit(onSubmit)}>
-      <fieldset className="search__fieldset">
-        <div className="search__container">
-          <input
-            className="search__input"
-            type="text"
-            placeholder="Enter topic"
-            {...register('searches', {
-              required: 'Please enter a keyword'
-            })}
-          />
-          {errors.searches && <span className="search__errors">{errors.searches.message}</span>}
-        </div>
-        <button
-          className="search__search"
-          style={{
-            backgroundColor: isButtonHovered ? '#2a65cc' : '#2f71e5'
-          }}
-          onMouseEnter={() => setIsButtonHovered(true)}
-          onMouseLeave={() => setIsButtonHovered(false)}>
-          Search
-        </button>
-      </fieldset>
+      {/* <fieldset className="search__fieldset"> */}
+      {/* <div className="search__container"> */}
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Enter topic"
+        {...register('searches', {
+          required: 'Please enter a keyword'
+        })}
+      />
+      {errors.searches && <span className="search__errors">{errors.searches.message}</span>}
+      {/* </div> */}
+      <button
+        className="search__search"
+        style={{
+          backgroundColor: isButtonHovered ? '#2a65cc' : '#2f71e5'
+        }}
+        onMouseEnter={() => setIsButtonHovered(true)}
+        onMouseLeave={() => setIsButtonHovered(false)}>
+        Search
+      </button>
+      {/* </fieldset> */}
       <button className="search__search-mobile">Search</button>
     </form>
   );
